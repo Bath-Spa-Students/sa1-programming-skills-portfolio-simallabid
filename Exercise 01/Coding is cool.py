d9002bc50a11c26c7190002e0df883999d57e816
simallabid <simalabid121488@gmail.com>
2024-10-15 13:06:53 +0400

    Create Coding is cool.py
    Coding is cool

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,4 @@
+word1 = "Coding "  #variable 1
+word2 = "Is "      #variable 2
+word3 = 'Cool'     #variable 3
+print(word1 + word2 + word3)  #using the string concatenation operator '+' to print it in a line
