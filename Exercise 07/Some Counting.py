37b6c5d67e622bec4df975fba0e3f16323a69471
simallabid <simalabid121488@gmail.com>
2024-11-07 12:08:31 +0400

    Some Counting

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,25 @@
+'''## Exercise 7: Some Counting - 20 Marks
+
+Use your newly acquired knowledge of the for loop to complete the following tasks. Print all values to the console
+in each case.
+* Write a loop that counts up from 0 to 50 in increments of 1.
+* Write a loop that counts down from 50 to 0 in decrements of 1.
+* Write a loop that counts up from 30 to 50 in increments of 1.
+* Write a loop that counts down from 50 to 10 in decrements of 2.
+* Write a loop that counts up from 100 to 200 in increments of 5.
+*You may include all loops in a single project*'''
+
+for a in range (0,50,1):  #range function from 0 to 50 with the increment of 1
+    print(a)              #print of range function 
+
+for b in range (50,0,-1): #range fuction from 50 to 0 with the decrement of 1
+    print(b)              #print of range function 
+    
+for c in range (30,50,1): #range function from 30 to 50 with the increment of 1
+    print(c)              #print of range function
+    
+for d in range (50,10,-2): #range function from 50 to 10 with the decrement of 1
+    print(d)               #print of range function
+    
+for e in range (100,200,5): #range function from 100 to 200 with the increment of 5
+    print(e)                #print of range function
