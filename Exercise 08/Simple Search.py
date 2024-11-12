d82f7377122882b6f770e78e0e76fe47b97a74b1
simallabid <simalabid121488@gmail.com>
2024-11-12 23:21:15 +0400

    Simple Search

---
+++ Exercise 08/Simple Search.py
@@ -0,0 +1,10 @@
+'''## Exercise 8: Simple Search - 30 Marks
+
+Write a program that searches for a specific string within a list of strings. The list is initialized with specific names ("Jake" "Zac", "Ian", "Ron", "Sam", "Dave"). , and your task is to search for "Sam".'''
+
+
+names = ['Jake', 'Zac', 'Ian', 'Ron', 'Sam', 'Dave']  #list of names
+if 'Sam' in names:                                    #finding if 'sam' is in the list
+    print('Sam is in the list')                       #output if 'sam' is in the list
+    
+    
