803abe129e5bb9b701288c3e5003cb0127258ce5
simallabid <simalabid121488@gmail.com>
2024-11-21 10:30:35 +0400

    Hello

--- Exercise 09/Hello.py
+++ Exercise 09/Hello.py
@@ -18,5 +18,5 @@ def hello():
 def main():
     hello()         #Fill in this blank to call the hello() function
 
-if __name__ == "__main__":
+if __name__ == "__main__": #makes sure that the function runs only when the script is executed
     main()
