a505ee489e8ec2bc9603f4994ff2cae82253b287
simallabid <simalabid121488@gmail.com>
2024-11-21 10:56:04 +0400

    Classwork python

---
+++ Classwork Python/Variables.py
@@ -0,0 +1,11 @@
+#variables
+a = "Hi"
+name = "My name is simal."
+age = "I am 18."
+b = "I am learning how to code."
+
+print(a , name , age)
+print(b)
+
+#comment
+"""multiple line comment"""
---
+++ Classwork Python/for loop.py
@@ -0,0 +1,16 @@
+'''for x in ['apple', "banana", "mango"]:
+    print(x)
+
+for x in range(0,15):
+    #print(x, end=' ')
+    print(x)
+    
+for x in range(0,15,3): #start,end,increment
+    print(x)
+    
+n= int(input('enter a num'))
+for v in range(0,n,2):
+    print(v)'''
+    
+animals =['cat', 'dog', 'bird', 'cow']
+print(animals[2:4])
