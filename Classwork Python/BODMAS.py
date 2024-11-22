5fe4e4140ab574310f88c5ad9acf214e98048e32
simallabid <simalabid121488@gmail.com>
2024-11-22 04:50:44 +0400

    Delete Classwork Python directory

--- Classwork Python/BODMAS.py
+++
@@ -1,7 +0,0 @@
-#operator precedence BODMAS
-
-print((2*6)/8 + 5*(8/4))
-
-#to write formula in multiple lines we need a back slash \
-print(8*3+3+2-8+ \
-    8/9-7*6)
--- Classwork Python/input.py
+++
@@ -1,7 +0,0 @@
-#to get an input from user.
-father_name = input("What is your father's name? ")
-print("My father's name is ", father_name)
-
-age = int(input("What is your age?"))   #it considers the input as string by default so we must mention it like this to change it.
-print("My age is", age)
-
