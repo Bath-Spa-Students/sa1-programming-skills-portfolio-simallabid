28eb1620d7d1a2bdc000b324e9bb0e2278cea358
simallabid <simalabid121488@gmail.com>
2024-11-22 04:43:18 +0400

    z_Classwork Python

---
+++ z_Classwork Python/List.py
@@ -0,0 +1,12 @@
+#lists can store different data types
+age = [17 , 18 , 56 , 45]  #homo
+print(age)
+
+colors = ['red', 'blue', 'white', 'yellow'] #homo
+print(colors)
+
+info = ["Simal", 18, 'red'] #hetro
+print(info)
+
+newinfo = info * 2
+print(newinfo)
---
+++ z_Classwork Python/String concatenation.py
@@ -0,0 +1,21 @@
+#string concatenation
+#it is a combination of two or more strings using +
+
+name= "Simal " + "Abid"                               #1st way
+print(name)
+
+course= "Creative " + "Computing"
+print(course)
+
+print('My ' + 'name ' + 'is ' + 'Simal')
+
+print('My', 'name', 'is', 'Simal')
+
+print('I' + " " + 'am ' + '18 ' + 'years ' + "old")   #2nd way
+
+a = "My fav song is"                                  #3rd way
+b= "Still"
+c= 'With'
+d= 'You'
+e= a + ' ' + b + ' ' + c + ' ' + d
+print (e) 
